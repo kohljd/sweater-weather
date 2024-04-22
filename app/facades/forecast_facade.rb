ddb6ac89c27e3ef77f6080cc712232b75e386edb
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-04-22 03:42:54 -0600

    forecast facade - location's weather

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -3,4 +3,9 @@ class ForecastFacade
     json = ForecastService.get_coordinates_for(location)
     coord = json[:results][0][:locations][0][:latLng]
   end
+
+  def self.get_weather_for(coordinates)
+    json = ForecastService.get_weather_for(coordinates)
+    Forecast.new(json)
+  end
 end
--- spec/facades/forecast_facade_spec.rb
+++ spec/facades/forecast_facade_spec.rb
@@ -20,4 +20,31 @@ RSpec.describe ForecastFacade do
       expect(coordinates[:lng]).to eq(-104.99202)
     end
   end
+
+  describe ".get_weather_for(location)" do
+    it "returns location's weather" do
+      coordinates = {lat: 39.74001, lng: -104.99202}
+      json_response = File.read("spec/fixtures/denver_forecast_2024-04-21.json")
+      stub_request(:get, "https://api.weatherapi.com/v1/forecast.json").
+        with(
+          query: {
+              key: Rails.application.credentials.weather[:api_key],
+              q: "#{coordinates[:lat]},#{coordinates[:lng]}",
+              days: 5,
+              aqi: "no",
+              alerts: "no"
+            }
+          ).
+        to_return(status: 200, body: json_response)
+
+      forecast = ForecastFacade.get_weather_for(coordinates)
+
+      expect(forecast).to be_a(Forecast)
+      expect(forecast.current_weather).to be_a(Hash)
+      expect(forecast.daily_weather).to be_an(Array)
+      expect(forecast.daily_weather).to all(be_a(Hash))
+      expect(forecast.hourly_weather).to be_an(Array)
+      expect(forecast.hourly_weather).to all(be_a(Hash))
+    end
+  end
 end

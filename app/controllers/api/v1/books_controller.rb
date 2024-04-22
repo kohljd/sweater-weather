0aa4e4c2398884e5f4294323c8f6a9ca870a17f5
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-04-22 10:32:29 -0500

    setup route and potential controller

---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::BooksController < ApplicationController
+  def index
+    coordinates = ForecastFacade.get_coordinates_for(params[:location])
+    forecast = ForecastFacade.get_forecast_for(coordinates)
+
+    book_results = BookFacade.get_book_results(params[:location], params[:quantity])
+    combined_result = BookFacade.combine_books_and_forecast(forecast, book_results)
+
+    render json: ForecastSerializer.new(combined_result)
+  end
+end
---
+++ app/poros/book_search.rb
@@ -0,0 +1,15 @@
+class Forecast
+  attr_reader :id,
+              :destination,
+              :forecast,
+              :total_books_found,
+              :books
+
+  def initialize(data)
+    @id = nil
+    @destination = 
+    @forecast = 
+    @total_books_found = 
+    @books = 
+  end
+end

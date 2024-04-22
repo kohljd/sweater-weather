d603914cf2d85c8b6bb8f7efd8d631b867879d3a
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-04-22 15:54:34 -0500

    test: user registration

---
+++ spec/requests/api/v0/user_registration_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.describe "POST /api/v0/users" do
+  it "registers a user" do
+    headers = {"Content_Type": "application/json", "Accept": "application/json"}
+    body = {
+      "email": "unknown@email.com",
+      "password": "the_most_secure_password",
+      "password_confirmation": "the_most_secure_password"
+    }
+
+    get "/api/v0/users", body: body, headers: headers
+
+    expect(response).to be_successful
+    expect(response.status).to eq(201)
+
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
+    user = parsed_response[:data]
+
+    expect(user[:id]).to eq(User.last.id)
+    expect(user[:type]).to eq("users")
+    expect(user[:attributes]).to include(:email, :api_key)
+  end
+end

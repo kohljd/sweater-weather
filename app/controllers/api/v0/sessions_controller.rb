cb2962b9ce20da4166ea70e7e74545522fdbc66d
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-04-23 13:45:44 -0500

    create session on successful login

--- app/controllers/api/v0/sessions_controller.rb
+++ app/controllers/api/v0/sessions_controller.rb
@@ -1,5 +1,10 @@
 class Api::V0::SessionsController < ApplicationController
   def create
-  
+    user = User.find_by!(email: params[:email])
+    if user&.authenticate(params[:password])
+      render json: UsersSerializer.new(user), status: :created
+    else
+      render json: { errors:[{ status: 422, detail: "Invalid credentials" }] }, status: :unprocessable_entity
+    end
   end
-endg
+end
--- spec/requests/api/v0/user_logs_in_spec.rb
+++ spec/requests/api/v0/user_logs_in_spec.rb
@@ -1,10 +1,9 @@
 require "rails_helper"
 
 RSpec.describe "POST /api/v0/sessions" do
-  let(:user) { User.create!(email: "unknown@email.com", password: "the_most_secure_password", password_confirmation: "the_most_secure_password", api_key: "ea2f6e7441fab8aa96f1611a0361c60d") }
+  let!(:user) { User.create!(email: "unknown@email.com", password: "the_most_secure_password", password_confirmation: "the_most_secure_password", api_key: "ea2f6e7441fab8aa96f1611a0361c60d") }
 
   it "creates session when user logs in" do
-    require 'pry'; binding.pry
     headers = {"Content_Type": "application/json", "Accept": "application/json"}
     body = {
       "email": "unknown@email.com",
@@ -19,13 +18,13 @@ RSpec.describe "POST /api/v0/sessions" do
     parsed_response = JSON.parse(response.body, symbolize_names: true)
     session = parsed_response[:data]
 
-    expect(session[:id]).to eq(user.id)
+    expect(session[:id].to_i).to eq(user.id)
     expect(session[:type]).to eq("users")
     expect(session[:attributes][:email]).to eq(user.email)
     expect(session[:attributes][:api_key]).to eq(user.api_key)
   end
 
-  it "must provide user email/password" do
+  it "must provide user email" do
     headers = {"Content_Type": "application/json", "Accept": "application/json"}
     body = {
       "email": "",
@@ -35,13 +34,31 @@ RSpec.describe "POST /api/v0/sessions" do
     post "/api/v0/sessions", params: body, headers: headers
 
     expect(response).to_not be_successful
-    expect(response.status).to eq(400)
+    expect(response.status).to eq(404)
 
     parsed_response = JSON.parse(response.body, symbolize_names: true)
     errors = parsed_response[:errors]
 
-    expect(errors[0][:status]).to eq(400)
-    expect(errors[0][:detail]).to eq("Validation failed: Email can't be blank, Email is invalid, Password can't be blank")
+    expect(errors[0][:status]).to eq(404)
+    expect(errors[0][:detail]).to eq("Couldn't find User")
+  end
+
+  it "must provide user password" do
+    headers = {"Content_Type": "application/json", "Accept": "application/json"}
+    body = {
+      "email": "unknown@email.com",
+      "password": "",
+    }
+
+    post "/api/v0/sessions", params: body, headers: headers
+    expect(response).to_not be_successful
+    expect(response.status).to eq(422)
+
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
+    errors = parsed_response[:errors]
+
+    expect(errors[0][:status]).to eq(422)
+    expect(errors[0][:detail]).to eq("Invalid credentials")
   end
 
   it "require valid user email" do
@@ -60,7 +77,7 @@ RSpec.describe "POST /api/v0/sessions" do
     errors = parsed_response[:errors]
 
     expect(errors[0][:status]).to eq(404)
-    expect(errors[0][:detail]).to eq("Validation failed: Password confirmation doesn't match Password")
+    expect(errors[0][:detail]).to eq("Couldn't find User")
   end
 
   it "require valid user password" do
@@ -73,12 +90,12 @@ RSpec.describe "POST /api/v0/sessions" do
     post "/api/v0/sessions", params: body, headers: headers
 
     expect(response).to_not be_successful
-    expect(response.status).to eq(400)
+    expect(response.status).to eq(422)
 
     parsed_response = JSON.parse(response.body, symbolize_names: true)
     errors = parsed_response[:errors]
 
-    expect(errors[0][:status]).to eq(400)
-    expect(errors[0][:detail]).to eq("Validation failed: Password is invalid")
+    expect(errors[0][:status]).to eq(422)
+    expect(errors[0][:detail]).to eq("Invalid credentials")
   end
 end

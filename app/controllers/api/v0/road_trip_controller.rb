f1c8ec8f29a51ce9cafcd24cc6a8716c146d1c21
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-04-23 14:59:39 -0500

    create road_trip route and controller

---
+++ app/controllers/api/v0/road_trip_controller.rb
@@ -0,0 +1,7 @@
+class Api::V0::RoadTripController < ApplicationController
+  def create
+    # req user's api_key
+    road_trip = RoadTripFacade.trip_details_for(params[:origin], params[:destination])
+    render json: RoadTripSerializer.new(road_trip)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
       resource :forecast, only: :show
       resources :users, only: :create
       resources :sessions, only: :create
+      resources :road_trip, only: :create
     end
   end
 end

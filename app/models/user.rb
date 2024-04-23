3fbe25a50a36be909359a4b338051456525f6b06
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-04-23 03:23:49 -0500

    update README, test registration sad path

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
   before_create :generate_api_key
 
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :password, presence: true
-  validates :password, confirmation: true, on: :create
+  validates :password_confirmation, presence: true, on: :create
   validates :api_key, uniqueness: true
 
   has_secure_password
@@ -14,5 +13,3 @@ class User < ApplicationRecord
     self.api_key = SecureRandom.hex(16)
   end
 end
-
-# , message: "must be a valid email address"
--- spec/requests/api/v0/user_registration_spec.rb
+++ spec/requests/api/v0/user_registration_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe "POST /api/v0/users" do
     expect(user[:attributes]).to include(:email, :api_key)
   end
 
-  it "user data must be valid" do
+  it "user data can't be blank" do
     headers = {"Content_Type": "application/json", "Accept": "application/json"}
     body = {
       "email": "",
@@ -39,7 +39,47 @@ RSpec.describe "POST /api/v0/users" do
     errors = parsed_response[:errors]
 
     expect(errors[0][:status]).to eq(400)
-    expect(errors[0][:detail]).to eq("Validation failed: Email can't be blank, Email is invalid, Password can't be blank, Password confirmation doesn't match Password, Password can't be blank")
+    expect(errors[0][:detail]).to eq("Validation failed: Email can't be blank, Email is invalid, Password confirmation can't be blank, Password can't be blank")
+  end
+
+  it "password and password confirmation must match" do
+    headers = {"Content_Type": "application/json", "Accept": "application/json"}
+    body = {
+      "email": "new_user@email.com",
+      "password": "an_unforgettable_password",
+      "password_confirmation": "that's_already_forgotten"
+    }
+
+    post "/api/v0/users", params: body, headers: headers
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
+
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
+    errors = parsed_response[:errors]
+
+    expect(errors[0][:status]).to eq(400)
+    expect(errors[0][:detail]).to eq("Validation failed: Password confirmation doesn't match Password")
+  end
+
+  it "must enter valid email format" do
+    headers = {"Content_Type": "application/json", "Accept": "application/json"}
+    body = {
+      "email": "new_user@",
+      "password": "original",
+      "password_confirmation": "original"
+    }
+
+    post "/api/v0/users", params: body, headers: headers
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
+
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
+    errors = parsed_response[:errors]
+
+    expect(errors[0][:status]).to eq(400)
+    expect(errors[0][:detail]).to eq("Validation failed: Email is invalid")
   end
 
   it "can't register user a second time" do
